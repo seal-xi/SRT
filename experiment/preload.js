ba172534022e97526d81e483f3bf5dca02793fcf
Zhao xiaoxi <88256108+seal-xi@users.noreply.github.com>
2025-06-02 21:50:25 +0800

    Update preload.js
    bug v1.0.2

--- experiment/preload.js
+++ experiment/preload.js
@@ -1,14 +1,17 @@
-const { contextBridge, ipcRenderer } = require('electron')
-const path = require('path')
-const os = require('os')
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
 
 // 安全地暴露API给渲染进程
-contextBridge.exposeInMainWorld(
-  'electron', {
-    saveFile: (data) => ipcRenderer.invoke('save-file', data),
-    createDirectory: (dirPath) => ipcRenderer.invoke('create-directory', dirPath),
-    getAppDataPath: () => {
-      return path.join(os.homedir(), '手语实验数据')
-    }
-  }
-)
+contextBridge.exposeInMainWorld('electronAPI', {
+  saveFile: (data) => ipcRenderer.invoke('save-file', data),
+  createDirectory: (dirPath) => ipcRenderer.invoke('create-directory', dirPath),
+  getAppPath: () => ipcRenderer.invoke('get-app-path')
+});
+
+// 将JSZip全局暴露，因为CSP阻止从CDN加载
+try {
+  const JSZip = require('jszip');
+  contextBridge.exposeInMainWorld('JSZip', JSZip);
+} catch (error) {
+  console.error('无法加载JSZip:', error);
+}
